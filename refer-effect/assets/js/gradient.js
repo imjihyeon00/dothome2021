1e4046f4fdf43c8f1bccd713d0594adc31c71d79
imjihyeon00 <imjihyeon00@gmail.com>
2021-09-01 11:06:48 +0900

    20210901

--- refer-effect/assets/js/gradient.js
+++ refer-effect/assets/js/gradient.js
@@ -1,24 +1,16 @@
 var renderer, scene, camera, composer, circle, skelet, particle;
 
 function init() {
-  renderer = new THREE.WebGLRenderer({
-    antialias: true,
-    alpha: true
-  });
-  renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1);
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setPixelRatio((window.devicePixelRatio) ? window.devicePixelRatio : 1);
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.autoClear = false;
   renderer.setClearColor(0x000000, 0.0);
-  document.getElementById("canvas").appendChild(renderer.domElement);
+  document.getElementById('canvas').appendChild(renderer.domElement);
 
   scene = new THREE.Scene();
 
-  camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    1,
-    1000
-  );
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
   camera.position.z = 400;
   scene.add(camera);
 
@@ -41,10 +33,8 @@ function init() {
 
   for (var i = 0; i < 1000; i++) {
     var mesh = new THREE.Mesh(geometry, material);
-    mesh.position
-      .set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5)
-      .normalize();
-    mesh.position.multiplyScalar(90 + Math.random() * 700);
+    mesh.position.set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5).normalize();
+    mesh.position.multiplyScalar(90 + (Math.random() * 700));
     mesh.rotation.set(Math.random() * 2, Math.random() * 2, Math.random() * 2);
     particle.add(mesh);
   }
@@ -58,6 +48,7 @@ function init() {
     color: 0xffffff,
     wireframe: true,
     side: THREE.DoubleSide
+
   });
 
   var planet = new THREE.Mesh(geom, mat);
@@ -68,22 +59,24 @@ function init() {
   planet2.scale.x = planet2.scale.y = planet2.scale.z = 10;
   skelet.add(planet2);
 
-  var ambientLight = new THREE.AmbientLight(0x999999);
+  var ambientLight = new THREE.AmbientLight(0x999999 );
   scene.add(ambientLight);
-
+  
   var lights = [];
-  lights[0] = new THREE.DirectionalLight(0xffffff, 1);
-  lights[0].position.set(1, 0, 0);
-  lights[1] = new THREE.DirectionalLight(0x11e8bb, 1);
-  lights[1].position.set(0.75, 1, 0.5);
-  lights[2] = new THREE.DirectionalLight(0x8200c9, 1);
-  lights[2].position.set(-0.75, -1, 0.5);
-  scene.add(lights[0]);
-  scene.add(lights[1]);
-  scene.add(lights[2]);
+lights[0] = new THREE.DirectionalLight( 0xffffff, 1 );
+lights[0].position.set( 1, 0, 0 );
+lights[1] = new THREE.DirectionalLight( 0x11E8BB, 1 );
+lights[1].position.set( 0.75, 1, 0.5 );
+lights[2] = new THREE.DirectionalLight( 0x8200C9, 1 );
+lights[2].position.set( -0.75, -1, 0.5 );
+scene.add( lights[0] );
+scene.add( lights[1] );
+scene.add( lights[2] );
+  
 
-  window.addEventListener("resize", onWindowResize, false);
-}
+  window.addEventListener('resize', onWindowResize, false);
+
+};
 
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -94,13 +87,13 @@ function onWindowResize() {
 function animate() {
   requestAnimationFrame(animate);
 
-  particle.rotation.x += 0.0;
-  particle.rotation.y -= 0.004;
-  circle.rotation.x -= 0.002;
-  circle.rotation.y -= 0.003;
-  skelet.rotation.x -= 0.001;
-  skelet.rotation.y += 0.002;
+  particle.rotation.x += 0.0000;
+  particle.rotation.y -= 0.0040;
+  circle.rotation.x -= 0.0020;
+  circle.rotation.y -= 0.0030;
+  skelet.rotation.x -= 0.0010;
+  skelet.rotation.y += 0.0020;
   renderer.clear();
 
-  renderer.render(scene, camera);
-}
+  renderer.render( scene, camera )
+};
