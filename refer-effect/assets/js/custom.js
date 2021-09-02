a2c31e55b587309383df926677932fcbe2d0a218
imjihyeon00 <imjihyeon00@gmail.com>
2021-09-02 16:33:20 +0900

    20210902
    tab menu 수업

---
+++ refer-effect/assets/js/custom.js
@@ -0,0 +1,35 @@
+//code view
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.querySelectorAll('pre code').forEach((el) => {
+    hljs.highlightElement(el);
+  });
+});
+
+//Code tab menu
+const viewBtn = document.querySelectorAll(".view-title ul li");
+const viewCont = document.querySelectorAll(".view-cont > div");
+
+viewBtn.forEach((btn, index) => {
+  btn.addEventListener("click", function () {
+    viewBtn.forEach(el => el.classList.remove("active"));
+    btn.classList.add("active");
+
+    viewCont.forEach(el => el.style.display = "none");
+    viewCont[index].style.display = "block";
+  });
+});
+
+//modal
+const infoBtn = document.querySelector(".info button");
+const modal = document.querySelector("#modal");
+const modalBtn = document.querySelector("#modal button");
+
+infoBtn.addEventListener("click", function () {
+  modal.classList.remove("show", "hide");
+  modal.classList.add("show");
+});
+
+modalBtn.addEventListener("click", function () {
+  modal.classList.remove("show", "hide");
+  modal.classList.add("hide");
+});
